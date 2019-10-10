fb965d679027bc5eece412d8394d562b28b43ecb
Janelle K Hirano <jaahnelle@gmail.com>
2019-10-09 20:10:12 -1000

    1-housekeeping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'angular-demo';
+  title = "angular-demo"; // state
+
+  // someData = 'someData';
+  // changeState(newState) {
+  //   this.someData = newState;
+  // }
 }
